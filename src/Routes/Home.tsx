4fded95d99942646c4f07abf4d02f994a2269aa1
raccoonboy <raccoonboy0803@gmail.com>
2024-05-17 16:41:29 +0900

    Feat: Home섹션 슬라이더 포스터 기능구현

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,13 +1,47 @@
+import { motion, AnimatePresence } from 'framer-motion';
+import { useState } from 'react';
 import { useQuery } from 'react-query';
 import styled from 'styled-components';
 import { getMovies, MovieDataProps } from '../api';
 import { makeImagePath } from '../utils';
+import useWindowDimensions from '../utils/windowResize';
 
 function Home() {
+  const windowWidth = useWindowDimensions();
+  const offset = 6;
   const { data, isLoading } = useQuery<MovieDataProps>(
     ['movies', 'nowPlaying'],
     getMovies
   );
+  // console.log('data:::', data);
+  const [index, setIndex] = useState(0);
+  const [leaving, setLeaving] = useState(false);
+
+  const increaseIndex = () => {
+    if (data) {
+      if (leaving) return;
+      toggleLeaving();
+      const totalMovies = data.results.length - 2;
+      const maxIndex = Math.ceil(totalMovies / offset);
+      console.log('index::', index);
+
+      setIndex((prev) => (prev === maxIndex - 1 ? 0 : prev + 1));
+    }
+  };
+
+  const toggleLeaving = () => setLeaving((prev) => !prev);
+
+  const rowVariants = {
+    hidden: {
+      x: windowWidth + 5,
+    },
+    visible: {
+      x: 0,
+    },
+    exit: {
+      x: -windowWidth - 5,
+    },
+  };
 
   return (
     <Wrapper>
@@ -16,11 +50,37 @@ function Home() {
       ) : (
         <>
           <Banner
+            onClick={increaseIndex}
             $bgImage={makeImagePath(data?.results[0].backdrop_path || '')}
           >
             <Title>{data?.results[0].title}</Title>
             <Overview>{data?.results[0].overview}</Overview>
           </Banner>
+          <Slider>
+            <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
+              <Row
+                key={index}
+                variants={rowVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                transition={{ type: 'tween', duration: 1 }}
+              >
+                {data?.results
+                  .slice(1, 19)
+                  .slice(offset * index, offset * index + offset)
+                  .map((movie) => (
+                    <Box
+                      key={movie.id}
+                      $bgImage={makeImagePath(
+                        movie.backdrop_path,
+                        'w500' || ''
+                      )}
+                    />
+                  ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
         </>
       )}
     </Wrapper>
@@ -58,4 +118,24 @@ const Title = styled.h2`
 const Overview = styled.p`
   width: 50%;
   font-size: 26px;
+`;
+
+const Slider = styled.div`
+  position: relative;
+  top: -150px;
+`;
+
+const Row = styled(motion.div)`
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  gap: 5px;
+  position: absolute;
+  width: 100%;
+`;
+
+const Box = styled(motion.div)<{ $bgImage: string }>`
+  background-image: url(${(props) => props.$bgImage});
+  background-size: cover;
+  background-position: center center;
+  height: 150px;
 `;
